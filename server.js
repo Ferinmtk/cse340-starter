cdd349767be9c52b65f4491eb8f16ce9b12a624d
ferinmtk <ferinmtk@gmail.com>
2025-05-25 22:26:40 +0300

    strings

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 app.set("view engine", "ejs")
@@ -22,6 +23,7 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
+app.get("/", baseController.buildHome)
 
 
 app.use(static)
